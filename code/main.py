c6e4c2477ac85cbef77feba1b169d22edc5df0f7
Ben Cartwright <bencart0905@utexas.edu>
2024-03-27 12:36:47 -0500

    revise

--- code/main.py
+++ code/main.py
@@ -8,8 +8,8 @@ import os
 
 
 shape_dict = {
-    "s1": [24*7*3, 24, 1024],
-    "s2": [24*7*3, 24*7, 1024],
+    "s1": [24*7, 24, 1024],
+    "s2": [24*7, 24*7, 1024],
 }
 station_list = ["Station1","Station6"]
 
@@ -20,7 +20,7 @@ for key, shape in shape_dict.items():
         for swc in swc_list:
             trial_num = 1
             trial = "Trial" + str(trial_num)
-            run_model(model_dict(shape = shape), trial, remove_met = False, test_station = station, target_col = swc, max_epochs = 1, patience = 5, trial_shape = shape)
+            run_model(model_dict(shape = shape), trial, remove_met = False, test_station = station, target_col = swc, max_epochs = 50, patience = 5, trial_shape = shape)
             trial_num += 1
 
 
