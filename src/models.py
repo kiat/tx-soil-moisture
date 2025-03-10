a5f7a3a7ec8abaeda25ac002140075eca00e5c20
Kia Teymourian <kiat@users.noreply.github.com>
2025-03-09 23:30:33 -0500

    .

--- src/models.py
+++ src/models.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Bidirectional, SimpleRNN, Conv1D, Flatten, Dense, InputLayer
+from tensorflow.keras.layers import Input, LSTM, Bidirectional, SimpleRNN, Conv1D, Flatten, Dense, InputLayer
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.losses import MeanSquaredError
@@ -17,7 +17,8 @@ from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_percenta
 
 def compile_lstm(input_shape):
     model = Sequential([
-        LSTM(32, input_shape=input_shape),
+        Input(shape=input_shape),
+        LSTM(32),
         Dense(32, activation='relu'),
         Dense(1, activation='linear')
     ])
@@ -26,7 +27,8 @@ def compile_lstm(input_shape):
 
 def compile_bilstm(input_shape):
     model = Sequential([
-        Bidirectional(LSTM(32, activation='tanh', input_shape=input_shape, return_sequences=True)),
+        Input(shape=input_shape),
+        Bidirectional(LSTM(32, activation='tanh', return_sequences=True)),
         Bidirectional(LSTM(16, return_sequences=True)),
         Bidirectional(LSTM(8, return_sequences=False)),
         Dense(8, activation='relu'),
